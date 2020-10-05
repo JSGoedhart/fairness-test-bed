594487883aeded7b63927ff32b622a37eaa64655
JSGoedhart <joosje.goedhart@student.uva.nl>
2020-10-05 20:54:19 +0200

    Run Propublica only (No lipschitz)

--- fairness/metrics/list.py
+++ fairness/metrics/list.py
@@ -32,7 +32,7 @@ metrics = [ Accuracy(), TPR(), TNR(), FPR(), FNR(), BCR(), MCC(), F1Score(),    
             SensitiveMetric(FPR), SensitiveMetric(FNR), # more group fairness metrics
             SensitiveMetric(CalibrationPos), SensitiveMetric(CalibrationNeg) ] # more group fairness metrics
 
-lipschitz = [LipschitzViolation(k, 'seuclidean') for k in range(1, 20)]
+# lipschitz = [LipschitzViolation(k, 'seuclidean') for k in range(1, 20)]
 
 METRICS = metrics + lipschitz
 
